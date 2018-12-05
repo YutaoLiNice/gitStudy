9a631dfc909c3c4533b8dfb575b7b4ec4e17d432
YutaoLiNice <941834816@qq.com>
2018-12-05 16:11:55 +0800

    gitStudy server change test1

--- src/main/java/gitStudy/Test.java
+++ src/main/java/gitStudy/Test.java
@@ -3,8 +3,11 @@ package gitStudy;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload");
+        System.out.println("upload");
 
+        System.out.println("upload");
+        System.out.println("upload");
 
 
 
