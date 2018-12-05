d0627074b63a50ec99946636f59c1f5e46575ee0
YutaoLiNice <941834816@qq.com>
2018-12-05 15:18:55 +0800

    Update Test.java
    sever change test

--- src/main/java/gitStudy/Test.java
+++ src/main/java/gitStudy/Test.java
@@ -10,6 +10,14 @@ public class Test {
         System.out.println("upload"); System.out.println("upload");
         System.out.println("upload"); System.out.println("upload");
         System.out.println("upload"); System.out.println("upload");
+        
+         System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
+
+        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
 
 
 
