176fb41e6a00db1fac3acb6e46d2d6b2a9b57f95
YutaoLiNice <941834816@qq.com>
2018-12-05 14:45:50 +0800

    gitStudy 4

--- src/main/java/gitStudy/Test.java
+++ src/main/java/gitStudy/Test.java
@@ -7,6 +7,9 @@ public class Test {
         System.out.println("upload"); System.out.println("upload");
         System.out.println("upload"); System.out.println("upload");
 
+        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
+        System.out.println("upload"); System.out.println("upload");
 
 
     }
