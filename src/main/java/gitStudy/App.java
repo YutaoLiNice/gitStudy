1eb3c11d727e2e6dc17f0fe61d8de3da787cfebf
YutaoLiNice <941834816@qq.com>
2018-12-05 14:02:57 +0800

    gitStudy first

--- src/main/java/gitStudy/App.java
+++ src/main/java/gitStudy/App.java
@@ -6,8 +6,10 @@ package gitStudy;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Git!" );
+
     }
 }
